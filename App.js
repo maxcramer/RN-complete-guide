00fa8bb9b0cf70edfa0e50b209a3d9d07c57680b
maxcramer <maximiliancramer@yahoo.com>
2020-11-13 14:36:13 +0000

    adding styling to input and button

--- App.js
+++ App.js
@@ -5,8 +5,10 @@ import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 export default function App() {
   return (
     <View style={{padding: 50}}>
-      <View>
-        <TextInput placeholder="Course Goals" style={{borderBottomColor: 'black', borderBottomWidth: 1}}/>
+      <View style={{flexDirection:'row', justifyContent:'space-between', alignItems:'center'}}>
+        <TextInput 
+        placeholder="Course Goals" 
+        style={{borderColor: 'black', borderWidth: 1, padding: 10, width: '80%'}}/>
         <Button title="➕"/>
       </View>
       <View>
